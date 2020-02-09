d0df42b7346dc56eada18bd84c8baf3abe1e99d2
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-09 13:09:01 -0800

    change mongodb URI

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
-// Configure middleware
-
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,7 +21,9 @@ var routes = require("./controllers/routes");
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapper";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // When the server starts, create and save a new Library document to the db
 // The "unique" rule in the Library model's schema will prevent duplicate libraries from being added to the server
