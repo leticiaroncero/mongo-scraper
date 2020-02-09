f6780f9fc35fd712ec22ef1e22670b53b6020c75
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-09 10:17:54 -0800

    add endpoint to retrieve, update and insert unique articles

--- controllers/routes.js
+++ controllers/routes.js
@@ -27,7 +27,6 @@ router.get("/api/fetch", function (req, res) {
         // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
         var $ = cheerio.load(response.data);
 
-        var articles = [];
         // With cheerio, find each p-tag with the "title" class
         // (i: iterator. element: the current element)
         $("a[data-click-id=body]").each(function (i, element) {
@@ -40,20 +39,18 @@ router.get("/api/fetch", function (req, res) {
             var link = $(element).attr("href");
 
             if (title && link) {
-                articles.push({
+                db.Article.findOneAndUpdate({
+                    link: link
+                }, {
                     title: title,
                     link: link
+                }, { upsert: true }).catch(function (err) {
+                    console.log("error inserting article");
                 });
             }
         });
 
-        db.Article.insertMany(articles).then(function (dbArticle) {
-            console.log("articles inserted");
-            res.status(200).end();
-        }).catch(function (err) {
-            console.log("error inserting articles");
-            res.status(400).end()
-        });
+        res.status(200).end();
     });
 });
 
